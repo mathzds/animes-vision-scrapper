74c48d7de1db7e0bf1001953352b902d3c1c0c62
mathzds <101145096+mathzds@users.noreply.github.com>
2023-10-09 14:13:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,16 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-// Middleware de CORS personalizado
-const allowCors = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  next();
-};
-
-app.use(allowCors);
+const port = process.env.PORT || 3000; // Define the port for your API
 
 const headers = {
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
 };
 
-async function fetchEpisodes(baseUrl, pageNumber) {
-  const pageUrl = `${baseUrl}?page=${pageNumber}`;
+async function fetchEpisodes(baseUrl) {
   try {
-    console.log(`Buscando página ${pageNumber}: ${pageUrl}`);
-    const response = await axios.get(pageUrl, { headers });
+    const response = await axios.get(baseUrl, { headers });
     if (response.status === 200) {
       const html = response.data;
       const $ = cheerio.load(html);
@@ -40,55 +24,48 @@ async function fetchEpisodes(baseUrl, pageNumber) {
           .siblings(".screen-item-info")
           .find("h3.sii-title")
           .text();
+        const imgSrc = $(element).find("img.sit-img").attr("src");
 
         if (href && href.includes("episodio")) {
-          itemLinks.push({ href, title });
+          itemLinks.push({ href, title, imgSrc });
         }
       });
 
       if (itemLinks.length === 0) {
-        return { pageNumber, data: [] };
+        return { data: [] };
       } else {
-        return { pageNumber, data: itemLinks };
+        return { data: itemLinks };
       }
     } else {
-      console.error(`Erro ao buscar página ${pageNumber}: Status ${response.status}`);
-      return { pageNumber, error: `Error: ${response.status}` };
+      return { error: `Error: ${response.status}` };
     }
   } catch (error) {
-    console.error(`Erro ao buscar página ${pageNumber}: ${error.message}`);
-    return { pageNumber, error: `Error: ${error.message}` };
+    return { error: `Error: ${error.message}` };
   }
 }
 
 app.get("/api/episodes", async (req, res) => {
   const baseUrl = req.query.url;
-  const maxPages = req.query.maxPages || 32;
 
   if (!baseUrl) {
     res.status(400).json({ error: "Missing 'url' parameter" });
     return;
   }
 
-  const promises = [];
-  for (let pageNumber = 1; pageNumber <= maxPages; pageNumber++) {
-    promises.push(fetchEpisodes(baseUrl, pageNumber));
-  }
-
   try {
-    const results = await Promise.all(promises);
-    const allEpisodes = results.reduce((acc, result) => {
-      if (result.error) {
-        throw new Error(result.error);
-      }
-      return acc.concat(result.data);
-    }, []);
-    res.json(allEpisodes);
+    const result = await fetchEpisodes(baseUrl);
+    
+    if (result.error) {
+      throw new Error(result.error);
+    }
+
+    res.json(result.data);
   } catch (error) {
-    console.error(`Erro interno do servidor: ${error.message}`);
-    res.status(500).json({ error: "Erro interno do servidor" });
+    res.status(500).json({ error: error.message });
   }
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
