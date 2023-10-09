fd295530b62771daf9027443c6f4f9d9bdc5bbc6
mathzds <101145096+mathzds@users.noreply.github.com>
2023-10-09 13:04:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const headers = {
   "User-Agent":
